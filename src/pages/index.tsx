5560f4e94c410abf2539d86ad3c6ed211c67ac6e
AshwariyaGopal <164196857+AshwariyaGopal@users.noreply.github.com>
2025-12-10 22:07:17 +0500

    fix: Update 'Textbook Overview' button link in hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ function HomepageHeader() {
           </Link>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/docs/Introduction/intro">
             Textbook Overview
           </Link>
         </div>
